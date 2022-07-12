d7b6e033463bf7d385ee24eae8ac6e35a71f26de
Ricco <Ricco.Ziegler@web.de>
2022-07-12 18:55:36 +0200

    _solve works for now

--- src/main/java/com/riccoz/backendsudoku/Sudoku.java
+++ src/main/java/com/riccoz/backendsudoku/Sudoku.java
@@ -42,8 +42,8 @@ public class Sudoku {
         return false;
     }
 
-    public boolean checkSudoku(int posX, int posY) {
-        if(checkSudoku_vertical(posY) && checkSudoku_horizontal(posX) && checkSudoku_square(posY, posX)) {
+    public boolean checkSudoku(int row, int column) {
+        if(checkSudoku_vertical(column) && checkSudoku_horizontal(row) && checkSudoku_square(row, column)) {
             return true;
         }
         return false;
@@ -81,15 +81,15 @@ public class Sudoku {
         return true;
     }
 
-    public boolean checkSudoku_horizontal(int column) {
+    public boolean checkSudoku_horizontal(int row) {
         ArrayList<Integer> list = new ArrayList<Integer>();
 
         for(int i = 0; i < 9; i++) {
-            if(list.contains(this.sudokuarray[column][i])) {
+            if(list.contains(this.sudokuarray[row][i])) {
                 return false;
             }
-            if(this.sudokuarray[column][i] != 0) {
-                list.add(this.sudokuarray[column][i]);
+            if(this.sudokuarray[row][i] != 0) {
+                list.add(this.sudokuarray[row][i]);
             }
 
         }
@@ -97,39 +97,44 @@ public class Sudoku {
         return true;
     }
 
-    public boolean checkSudoku_vertical(int row) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
+    public boolean checkSudoku_vertical(int column) {
+        ArrayList<Integer> list = new ArrayList<>();
 
         for(int i = 0; i < 9; i++) {
-            if(list.contains(this.sudokuarray[i][row])) {
+            if(list.contains(this.sudokuarray[i][column])) {
                 return false;
             }
-            if(this.sudokuarray[i][row] != 0) {
-                list.add(this.sudokuarray[i][row]);
+            if(this.sudokuarray[i][column] != 0) {
+                list.add(this.sudokuarray[i][column]);
             }
         }
 
         return true;
     }
 
-    public boolean checkSudoku_square(int posX, int posY) {
+    public boolean checkSudoku_square(int row, int column) {
         ArrayList<Integer> list = new ArrayList<Integer>();
 
-        int startX = (posX / 3) * 3;
-        int startY = (posY / 3) * 3;
+        int startRow = (row / 3) * 3;
+        int startCol = (column / 3) * 3;
 
-        for(int i = startX; i <= startX+2; i++) {
-            for(int z = startY; z <= startY+2; z++) {
-                if(list.contains(this.sudokuarray[i][z])) {
+        for(int col = startCol; col <= startCol+2; col++) {
+            for(int row_ = startRow; row_ <= startRow+2; row_++) {
+                if(list.contains(this.sudokuarray[row_][col])) {
                     return false;
                 }
-                if(this.sudokuarray[i][z] != 0) {
-                    list.add(this.sudokuarray[i][z]);
+                if(this.sudokuarray[row_][col] != 0) {
+                    list.add(this.sudokuarray[row_][col]);
                 }
             }
         }
 
         return true;
+    }
+
+    public boolean checkSudoku_full_position(Sudoku sudoku, int row, int column, int number) {
+        sudoku.sudokuarray[row][column] = number;
+        return sudoku.checkSudoku(row,column);
     }
 
     public Sudoku solve() {
@@ -144,19 +149,18 @@ public class Sudoku {
         return null;
     }
 
-    public boolean _solve(Sudoku array, int posY, int posX) {
-        if(posX > 8) {
-            posX = 0;
-            posY++;
+    public boolean _solve(Sudoku array, int row, int column) {
+        if(column > 8) {
+            column = 0;
+            row++;
         }
 
-        for (int i = posY; i < array.sudokuarray.length; i++) {
-            for (int z = posX; z < array.sudokuarray.length; z++) {
+        for (int i = row; i < array.sudokuarray.length; i++) {
+            for (int z = column; z < array.sudokuarray.length; z++) {
                 if(array.sudokuarray[i][z] == 0) {
                     for(int num = 1; num <= 9 ; num++) {
-                        array.sudokuarray[i][z] = num;
                         display();
-                        if(checkSudoku(i,z)) {
+                        if(checkSudoku_full_position(this, i, z, num)) {
                             System.out.println("Darf, also gehts weiter!");
                             if(_solve(array,i ,z+1)) {
                                 return true;
